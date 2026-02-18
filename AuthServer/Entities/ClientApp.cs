1d7d0268324ca2bba0661011365551c89bcbe449
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-02-18 23:33:30 +0530

    Implement the ClientApp model class.

---
+++ AuthServer/Entities/ClientApp.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.Entities
+{
+    public class ClientApp
+    {
+        public Guid Id { get; set; }
+        [MaxLength(100)]
+        public string ClientId { get; set; } = default!;
+        [MaxLength(200)]
+        public string Name { get; set; } = default!;
+        [MaxLength(200)]
+        public string ClientSecretHash { get; set; } = default!;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+
+    }
+}

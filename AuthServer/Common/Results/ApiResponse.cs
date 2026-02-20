b4a840e2918f0e9e70738adff3a356a350cfb99e
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-02-20 23:24:35 +0530

    feat(common): implement standardized ApiResponse wrapper for consistent API responses

---
+++ AuthServer/Common/Results/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace AuthServer.Common.Results
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; } = "OK";
+
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> Success(T data,string message = "OK")
+        {
+            return new ApiResponse<T>() { IsSuccess = true, Message = message, Data = data };
+        }
+
+        public static ApiResponse<T> Fail(string message)
+        {
+            return new ApiResponse<T> { IsSuccess = false, Message = message, };
+        }
+    }
+}

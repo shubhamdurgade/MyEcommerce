2e76d64e7f85cc58948296c26d0b5da9a6ac130b
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-02-23 23:56:37 +0530

    feat(auth): implement Logout and Refresh request DTOs

---
+++ AuthServer/DTOs/Requests/LogoutRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.DTOs.Requests
+{
+    public class LogoutRequestDTO
+    {
+        [Required(ErrorMessage = "ClientId is required")]
+        public string ClientId { get; set; } = default!;
+
+        [Required(ErrorMessage = "ClientSecret is required")]
+        public string ClientSecret { get; set; } = default!;
+
+        [Required(ErrorMessage = "RefreshToken is required")]
+        public string RefreshToken { get; set; } = default!;
+
+        [Required(ErrorMessage = "DeviceId is required")]
+        public string DeviceId { get; set; } = default!; 
+    }
+}
---
+++ AuthServer/DTOs/Requests/RefreshRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.DTOs.Requests
+{
+    public class RefreshRequestDTO
+    {
+        [Required(ErrorMessage = "ClientId is required")]
+        public string ClientId { get; set; } = default!;
+
+        [Required(ErrorMessage = "ClientSecret is required")]
+        public string ClientSecret { get; set; } = default!;
+
+        [Required(ErrorMessage = "RefreshToken is required")]
+        public string RefreshToken { get; set; } = default!; 
+    }
+}

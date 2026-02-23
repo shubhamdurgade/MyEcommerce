d5959389c936064f4eddb30f6796605a0739c290
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-02-23 23:50:13 +0530

    feat(auth): implement Login and Register request DTOs

---
+++ AuthServer/DTOs/Requests/LoginRequestDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.DTOs.Requests
+{
+    public class LoginRequestDTO
+    {
+        [Required(ErrorMessage = "ClientId is required.")]
+        [StringLength(100, ErrorMessage = "ClientId cannot exceed 100 characters.")]
+        public string ClientId { get; set; } = default!;
+
+        [Required(ErrorMessage = "ClientSecret is required.")]
+        [StringLength(100, ErrorMessage = "ClientSecret cannot exceed 100 characters.")]
+        public string ClientSecret { get; set; } = default!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(100, ErrorMessage = "Please enter valid Email address")]
+        public string Email { get; set; } = default!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must between 6 and 100 charactres")]
+        public string Password { get; set; } = default!;
+
+        [Required(ErrorMessage = "DeviceId is required.")]
+        [StringLength(100, ErrorMessage ="DeviceId cannot exceed 100 characters")]
+        public string DeviceId { get; set; } = default!;
+    }
+}
---
+++ AuthServer/DTOs/Requests/RegisterRequestDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.DTOs.Requests
+{
+    public class RegisterRequestDTO
+    {
+        [Required(ErrorMessage = "FirstName is required.")]
+        [StringLength(100, ErrorMessage = "FirstName cannot exceed 100 characters")]
+        public string FirstName { get; set; } = default!;
+
+        [Required(ErrorMessage = "LastName is required.")]
+        [StringLength(100, ErrorMessage = "LastName cannot exceed 100 characters")]
+        public string LastName { get; set; } = default!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(100, ErrorMessage = "Please enter valid Email address")]
+        public string Email { get; set; } = default!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6,ErrorMessage = "Password must between 6 and 100 charactres")]
+        public string Password { get; set; } = default!;
+
+        [Compare(nameof(Password),ErrorMessage ="ConfirmPassword must match Password.")]
+        public string ConfirmPassword { get; set; } = default!;
+
+        [StringLength(20, ErrorMessage ="PhoneNumber cannot exceed 20 characters")]
+        public string? PhoneNumber { get; set; }    
+    }
+}
